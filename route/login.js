b9b43197d2a8842f6686482de6f1b10e6b84c950
joelakakpo <jeeakakpo@gmail.com>
2024-12-17 21:47:27 +0000

    Replace bcrypt with bcryptjs in login route for improved compatibility

--- route/login.js
+++ route/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../modals/User") // Ensure this path is correct
 
 // Login Route
